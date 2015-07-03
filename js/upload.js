5f207744e2a36070c190d47cd84c7fa18e52403a
dev_Scott <60510scott@gmail.com>
2015-07-03 09:12:05 +0800

    FB auth

--- js/upload.js
+++ js/upload.js
@@ -1,13 +1,23 @@
 ﻿window.fbAsyncInit = function () {
-    var uname;
-    FB.getLoginStatus(function (response) {
-        if (response.status === 'connected') {
-            FB.api('/me', function (response) { uname = response.name; });
-            // var uid = response.authResponse.userID;
-            // var accessToken = response.authResponse.accessToken;
-        } else uname = Parse.User.current().getUsername();
-        $('.dropdown-toggle').append(uname + ' <span class="caret">');
+    Parse.$ = jQuery;
+    Parse.initialize("kMUH1stxvfuI5IxWHoA8x3rCaEqBWYgNUx5Wembu", "nSJVzXIq2iSBdUUBvLKnpW4okjgZ8SV0Dq3E1IFi");
+    Parse.FacebookUtils.init({
+        appId: '1675828652651130', // Facebook App ID
+        status: true,  // check Facebook Login status
+        cookie: true,  // enable cookies to allow Parse to access the session
+        xfbml: true,  // initialize Facebook social plugins on the page
+        version: 'v2.3' // Facebook Graph API version
     });
+    if (Parse.User.current()) {
+        var uname;
+        if (Parse.FacebookUtils.isLinked(Parse.User.current()))
+            FB.api('/me', function (response) { uname = response.name; });
+        else uname = Parse.User.current().getUsername();
+        $('.dropdown-toggle').append(uname + ' <span class="caret">');
+    } else {
+        window.stop();
+        window.location.href = './login.html';
+    }
 };
 
 (function (d, s, id) {
@@ -19,12 +29,6 @@
 }(document, 'script', 'facebook-jssdk'));
 
 $(function () {
-    Parse.$ = jQuery;
-    Parse.initialize("kMUH1stxvfuI5IxWHoA8x3rCaEqBWYgNUx5Wembu", "nSJVzXIq2iSBdUUBvLKnpW4okjgZ8SV0Dq3E1IFi");
-    if (!Parse.User.current()) {
-        window.stop();
-        window.location.href = './login.html';
-    }
     $('.alert').hide();
     $("#photo_front").fileinput({
         showUpload: false,
