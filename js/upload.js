2e7aa3c4f2830c5c14a831283cb72d1a355e513c
dev_Scott <60510scott@gmail.com>
2015-07-03 10:53:21 +0800

    FB bugs

--- js/upload.js
+++ js/upload.js
@@ -1,34 +1,41 @@
 ﻿window.fbAsyncInit = function () {
-    Parse.$ = jQuery;
-    Parse.initialize("kMUH1stxvfuI5IxWHoA8x3rCaEqBWYgNUx5Wembu", "nSJVzXIq2iSBdUUBvLKnpW4okjgZ8SV0Dq3E1IFi");
     Parse.FacebookUtils.init({
         appId: '1675828652651130', // Facebook App ID
         status: false,  // check Facebook Login status
         cookie: true,  // enable cookies to allow Parse to access the session
         xfbml: true,  // initialize Facebook social plugins on the page
-        version: 'v1' // Facebook Graph API version
+        version: 'v2.3' // Facebook Graph API version
     });
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/zh_TW/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    if (Parse.User.current()) {
-        var uname;
-        if (Parse.FacebookUtils.isLinked(Parse.User.current()))
-            FB.api('/me', function (response) { uname = response.name; });
-        else uname = Parse.User.current().getUsername();
-        $('.dropdown-toggle').append(uname + ' <span class="caret">');
-    } else {
-        window.stop();
-        window.location.href = './login.html';
-    }
+    $(document).trigger('FBSDKLoaded');
 };
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/zh_TW/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 
 $(function () {
+    Parse.$ = jQuery;
+    Parse.initialize("kMUH1stxvfuI5IxWHoA8x3rCaEqBWYgNUx5Wembu", "nSJVzXIq2iSBdUUBvLKnpW4okjgZ8SV0Dq3E1IFi");
+
+    $(document).bind('FBSDKLoaded', function () {
+        console.log('FB loaded')
+        if (Parse.User.current()) {
+            var uname;
+            if (Parse.FacebookUtils.isLinked(Parse.User.current()))
+                FB.api('/me', function (response) { uname = response.name; });
+            else uname = Parse.User.current().getUsername();
+            $('.dropdown-toggle').append(uname + ' <span class="caret">');
+        } else {
+            window.stop();
+            window.location.href = './login.html';
+        }
+    });
+
     $('.alert').hide();
     $("#photo_front").fileinput({
         showUpload: false,
