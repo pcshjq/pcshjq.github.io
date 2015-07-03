f5c9b70e459a09f2a9ce35932335b6691818af10
dev_Scott <60510scott@gmail.com>
2015-07-03 08:58:06 +0800

    FB bugs

--- js/upload.js
+++ js/upload.js
@@ -6,6 +6,15 @@
         xfbml: true,  // initialize Facebook social plugins on the page
         version: 'v2.3' // Facebook Graph API version
     });
+    var uname;
+    FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+            FB.api('/me', function (response) { uname = response.name; });
+            // var uid = response.authResponse.userID;
+            // var accessToken = response.authResponse.accessToken;
+        } else uname = Parse.User.current().getUsername();
+    });
+    $('.dropdown-toggle').append(uname + ' <span class="caret">');
 };
 
 (function (d, s, id) {
@@ -22,17 +31,6 @@ $(function () {
     if (!Parse.User.current()) {
         window.stop();
         window.location.href = './login.html';
-    }
-    else {
-        var uname;
-        FB.getLoginStatus(function (response) {
-            if (response.status === 'connected') {
-                FB.api('/me', function (response) { uname = response.name; });
-                // var uid = response.authResponse.userID;
-                // var accessToken = response.authResponse.accessToken;
-            } else uname = Parse.User.current().getUsername();
-        });
-        $('.dropdown-toggle').append(uname + ' <span class="caret">');
     }
     $('.alert').hide();
     $("#photo_front").fileinput({
