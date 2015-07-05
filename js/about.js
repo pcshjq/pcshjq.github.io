2dd2656aafea96799f79e9831dc42683355cdb50
dev_Scott <60510scott@gmail.com>
2015-07-05 10:34:21 +0800

    policy

--- js/about.js
+++ js/about.js
@@ -36,4 +36,7 @@ $(function () {
         window.stop();
         window.location.href = './login.html';
     }
+    $("#collapse1").load("dcl/Privacy_Policy.txt");
+    $("#collapse2").load("dcl/MIT.txt");
+    $("#collapse3").load("dcl/BSD.txt");
 });
